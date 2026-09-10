b86d092fe4d77411fc706df42a1b00ed893d808b
ufatehh0 <ufateh2006@gmail.com>
2026-09-11 03:57:29 +0400

    feat(save-system): implement secret code developer cheats listener

---
+++ game/Assets/Scripts/GameManager/SecretCodeManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SecretCodeManager : MonoBehaviour
+{
+    private string secretCode = "RAHMAN";
+    private string input = "";
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            input += c.ToString().ToUpper(); // Karakteri ekle ve büyük harfe çevir
+
+            // Eğer input secretCode uzunluğundan büyük olursa baştan kes
+            if (input.Length > secretCode.Length)
+            {
+                input = input.Substring(input.Length - secretCode.Length);
+            }
+
+            // Şifre doğruysa fonksiyonu tetikle
+            if (input == secretCode)
+            {
+                TriggerSecretFunction();
+                input = ""; // Tekrar tetiklememesi için sıfırla
+            }
+        }
+    }
+
+    void TriggerSecretFunction()
+    {
+        SnakeSizeManager.Instance.SetBigSize(100000f);
+    }
+}
