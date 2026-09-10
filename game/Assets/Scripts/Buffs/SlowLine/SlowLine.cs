41f0902baae0f2fa58583fa9eecf07f95f5bb7ee
ufatehh0 <ufateh2006@gmail.com>
2026-09-11 03:56:37 +0400

    feat(combat): implement slow line resistance logic

---
+++ game/Assets/Scripts/Buffs/SlowLine/SlowLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowLine : MonoBehaviour
+{
+    public Vector2 offsetChange;
+
+    private Renderer objRenderer;
+
+    void Start()
+    {
+        objRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        // Offset'i sürekli kaydırmak
+        objRenderer.material.mainTextureOffset += offsetChange * Time.deltaTime;
+    }
+}
