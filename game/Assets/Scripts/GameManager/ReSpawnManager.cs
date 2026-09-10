866fd2b0596d8e51709fcc58e045ccb255b935d3
ufatehh0 <ufateh2006@gmail.com>
2026-09-11 03:57:28 +0400

    feat(save-system): implement respawn manager checkpoint restore logic

---
+++ game/Assets/Scripts/GameManager/ReSpawnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReSpawnManager : MonoBehaviour
+{
+    public static ReSpawnManager Instance;
+
+  
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SpawnerTimer()
+    {
+        StartCoroutine(FreezeTimeCoroutine());
+    }
+
+    private IEnumerator FreezeTimeCoroutine()
+    {
+        Fade.Instance.whiteFade.gameObject.SetActive(true);
+        Fade.Instance.whiteFade.raycastTarget = true;
+        Fade.Instance.whiteFade.canvasRenderer.SetAlpha(1f);
+
+        // 0.5 saniye beyaz ekran
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        // Zamanı durdur
+        TimeManager.Instance.StopTime();
+
+        // freezeTimeAfterRewind boyunca bekle
+        yield return new WaitForSecondsRealtime(0.3f);
+
+        // Zamanı başlat
+        TimeManager.Instance.StartTime();
+
+        // FadeOut yap (beyaz kaybolsun)
+        Fade.Instance.FadeIn();
+
+        Fade.Instance.whiteFade.raycastTarget = false;
+
+        EnvColor.Instance.ChangeColor();
+        ScoreManager.Instance.ResumeScore();
+
+    }
+}
