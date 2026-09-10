bb88dea3c84bd2ad111f4cf7f12fd9a57dd1d85c
ufatehh0 <ufateh2006@gmail.com>
2026-09-11 03:57:04 +0400

    feat(ui): implement level complete banner animator

---
+++ game/Assets/Scripts/Level-Text/LevelTextManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelTextManager : MonoBehaviour
+{
+    public static LevelTextManager Instance;
+    public TMP_Text levelText;
+
+    public int level = 1;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Kaydedilmiş level varsa onu yükle
+        level = PlayerPrefs.GetInt("PlayerLevel", 1);
+        UpdateText();
+    }
+
+    void Update()
+    {
+        if (AppleManager.Instance.apple >= 10 * level)
+        {
+            Debug.Log("Next Level");
+            level += 1;
+
+            // Yeni leveli kaydet
+            PlayerPrefs.SetInt("PlayerLevel", level);
+            PlayerPrefs.Save();
+
+            UpdateText();
+
+
+            ComplateLevel();
+        }
+    }
+
+    public void UpdateText()
+    {
+        levelText.text = "Level : " + level;
+    }
+
+    public void ComplateLevel()
+    {
+        UIManager.Instance.gameUI.SetActive(false);
+        GameOverManager.Instance.gameOver = true;
+        CameraManager.Instance.follow = false;
+        ScoreManager.Instance.StopScore();
+        UIManager.Instance.NextLevelUI.SetActive(true);
+        UIManager.Instance.Conffeti.SetActive(true);
+        SoundManager.Instance.PlaySound(SoundType.Win);
+    }
+}
