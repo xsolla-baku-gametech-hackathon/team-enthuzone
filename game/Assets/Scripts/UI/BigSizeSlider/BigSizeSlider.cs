5500899a1be4cff67f4ec52e1c8fb8825e2d2aa7
ufatehh0 <ufateh2006@gmail.com>
2026-09-11 03:57:03 +0400

    feat(ui): implement giant mode duration countdown slider

---
+++ game/Assets/Scripts/UI/BigSizeSlider/BigSizeSlider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BigSizeSlider : MonoBehaviour
+{
+    public static BigSizeSlider Instance;
+    public Slider slider;
+
+    public float maxValue = 100f;
+
+    private Tween sliderTween;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void InitializeSlider(float duration)
+    {
+        // Önceki animasyonu iptal et
+        if (sliderTween != null && sliderTween.IsActive())
+            sliderTween.Kill();
+
+        // Baştan başlasın
+        slider.maxValue = maxValue;
+        slider.value = maxValue;
+
+        // Yeni tween başlat
+        sliderTween = slider.DOValue(0f, duration)
+    .SetEase(Ease.Linear)
+    .OnComplete(() =>
+    {
+       
+        SnakeSizeManager.Instance.SetNormalSize(); // Animasyon bittiğinde çağrılır
+    });
+    }
+}
