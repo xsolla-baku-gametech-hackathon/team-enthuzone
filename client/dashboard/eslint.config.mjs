fa97e17881b1fd7626eaac7ea3515f175b3a4aeb
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:25:09 +0400

    chore(eslint): initialize flat eslint configuration

---
+++ client/dashboard/eslint.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
