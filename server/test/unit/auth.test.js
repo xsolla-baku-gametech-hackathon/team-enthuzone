0a50550f569ed065e1f422cc8b83473faf09a03a
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:06 +0400

    test(unit): create unit tests for password hashing and verification

---
+++ server/test/unit/auth.test.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-secret-at-least-32-characters-long';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { createAuthModule, MemoryTransactionManager, requireAnyRole } = require('../../src/modules/auth');
+const { MemoryOrganizationRepository } = require('../../src/modules/organization');
+const { MemoryUserRepository } = require('../../src/modules/user');
+
+test('registration transaction rolls organization back when user creation fails', async () => {
+  class FailingUserRepository extends MemoryUserRepository {
+    async create() { throw new Error('simulated user write failure'); }
