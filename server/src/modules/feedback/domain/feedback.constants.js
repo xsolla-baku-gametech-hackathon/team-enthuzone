c7040b8de17f32c5f36d8403022eaf94e1fd2fae
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:41 +0400

    feat(feedback): declare STEAM, DISCORD, REDDIT, SUPPORT sources

--- server/src/modules/feedback/domain/feedback.constants.js
+++ server/src/modules/feedback/domain/feedback.constants.js
@@ -3,4 +3,11 @@ const FEEDBACK_SOURCES = Object.freeze([
   'DISCORD',
   'REDDIT',
   'SUPPORT',
-]);
+]);
+
+const SENTIMENTS = Object.freeze(['POSITIVE', 'NEUTRAL', 'NEGATIVE']);
+
+const ISSUE_CATEGORIES = Object.freeze([
+  'GAMEPLAY_BALANCE',
+  'PERFORMANCE',
+  'MATCHMAKING',
