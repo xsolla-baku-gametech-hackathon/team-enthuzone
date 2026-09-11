dbf438654320466b72ef37423361d1bae24382b1
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:37:02 +0400

    refactor(org): add validateMemberRole permission check (iteration 2)

--- server/src/modules/organization/application/organization.service.js
+++ server/src/modules/organization/application/organization.service.js
@@ -6,4 +6,9 @@ class OrganizationService {
     this.organizationRepository = organizationRepository;
   }
 
-  async create({ name }, transaction) {
+  async create({ name }, transaction) {
+    const baseSlug = slugifyOrganizationName(name);
+    let slug = baseSlug;
+
+    for (let suffix = 1; suffix <= 100; suffix += 1) {
+      if (!(await this.organizationRepository.existsBySlug(slug, transaction))) {
