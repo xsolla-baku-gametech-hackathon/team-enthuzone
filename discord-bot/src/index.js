6cb8293af3a18a170f12196e8f4ae12bea17f75f
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 05:00:18 +0400

    refactor(client): handle clientReady asynchronous event setup

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -60,3 +60,4 @@ async function relay(message) {
         body: JSON.stringify({
           id: message.id,
           author: message.author.tag || message.author.username || message.author.id,
+          content: message.content.slice(0, 6000),
