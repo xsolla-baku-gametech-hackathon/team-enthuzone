e7a27fefe368dcf118376693053c561b3a1fb420
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:11 +0400

    fix(database): fix unhandled promise rejections on initial connect failure

--- server/src/infrastructure/database/mongoose.connection.js
+++ server/src/infrastructure/database/mongoose.connection.js
@@ -4,4 +4,6 @@ async function connectMongo(uri) {
   mongoose.set('strictQuery', true);
   mongoose.set('bufferCommands', false);
   await mongoose.connect(uri, {
-    serverSelectionTimeoutMS: 5000,
+    serverSelectionTimeoutMS: 5000,
+    maxPoolSize: 20,
+    minPoolSize: 1,
