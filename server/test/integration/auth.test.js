cbcc642c2f7968c1474fbd3bc426913c7b0ed02c
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:09 +0400

    test(integration): test complete registration and login http workflow

---
+++ server/test/integration/auth.test.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'test-secret-at-least-32-characters-long';
+
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const jwt = require('jsonwebtoken');
+const request = require('supertest');
+const { createApp } = require('../../src/app');
+const { MemoryOrganizationRepository } = require('../../src/modules/organization');
+const { MemoryUserRepository } = require('../../src/modules/user');
+const { MemoryTransactionManager } = require('../../src/modules/auth');
+
