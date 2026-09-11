00bf109ef195d1ad81d7c8445d687ff70995d4d3
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:12 +0400

    fix(telemetry): handle empty telemetry batch arrays gracefully

--- server/src/modules/telemetry/presentation/http/telemetry.controller.js
+++ server/src/modules/telemetry/presentation/http/telemetry.controller.js
@@ -8,4 +8,15 @@ class TelemetryController {
     res.status(202).json({ telemetry });
   };
 
-  createBatch = async (req, res) => {
+  createBatch = async (req, res) => {
+    const telemetry = await this.telemetryService.ingestBatch(req.body.events);
+    res.status(202).json({ accepted: telemetry.length, telemetry });
+  };
+
+  list = async (req, res) => {
+    const telemetry = await this.telemetryService.list(req.query);
+    res.json({ telemetry, count: telemetry.length, limit: req.query.limit, offset: req.query.offset });
+  };
+}
+
+module.exports = { TelemetryController };
