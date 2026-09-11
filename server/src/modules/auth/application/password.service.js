1d9b98e5568b1344e70d1958fceb6a907863077e
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:35:40 +0400

    feat(auth): implement comparePassword with salt timing protection

--- server/src/modules/auth/application/password.service.js
+++ server/src/modules/auth/application/password.service.js
@@ -3,4 +3,10 @@ const bcrypt = require('bcryptjs');
 class PasswordService {
   constructor({ rounds = 12 } = {}) {
     this.rounds = rounds;
-  }
+  }
+
+  hash(password) {
+    return bcrypt.hash(password, this.rounds);
+  }
+
+  compare(password, passwordHash) {
