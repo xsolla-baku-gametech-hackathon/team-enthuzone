e679fad79cd449b16ffc7da88af633461734d00c
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:04 +0400

    feat(server): connect to MongoDB before accepting incoming traffic

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,9 @@
 const { createApp } = require('./app');
 const { env } = require('./config/environment');
 const { connectMongo, disconnectMongo } = require('./infrastructure/database/mongoose.connection');
-const { MongoFeedbackRepository } = require('./modules/feedback');
+const { MongoFeedbackRepository } = require('./modules/feedback');
+const { MongoTelemetryRepository } = require('./modules/telemetry');
+const { MongoOrganizationRepository } = require('./modules/organization');
+const { MongoUserRepository } = require('./modules/user');
+const { MongoTransactionManager } = require('./modules/auth');
+
