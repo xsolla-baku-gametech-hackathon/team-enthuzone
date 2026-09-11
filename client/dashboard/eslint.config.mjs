8cbaee84edb9398c0944c798594ebf43f439484e
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:25:10 +0400

    chore(eslint): setup fileURLToPath dirname resolver

--- client/dashboard/eslint.config.mjs
+++ client/dashboard/eslint.config.mjs
@@ -3,4 +3,10 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
