6e5c191b51d0c2b62411ee2944aa149998680193
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:33 +0400

    feat(auth): hash user password with 12 salt rounds

--- server/src/modules/auth/application/auth.service.js
+++ server/src/modules/auth/application/auth.service.js
@@ -22,4 +22,11 @@ class AuthService {
       throw new ConflictError('A user with this email already exists');
     }
 
-    const passwordHash = await this.passwordService.hash(input.password);
+    const passwordHash = await this.passwordService.hash(input.password);
+    let created;
+    for (let attempt = 0; attempt < 3; attempt += 1) {
+      try {
+        created = await this.transactionManager.execute(async (transaction) => {
+          if (await this.userService.findByEmail(email, transaction)) {
+            throw Object.assign(new Error('Duplicate user email'), { code: 'DUPLICATE_EMAIL' });
+          }
