74f89ade617e50b5601d9c676c4b23df652a23d2
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 05:02:19 +0400

    refactor(client): handle clientReady asynchronous event setup (iteration 16)

--- discord-bot/src/index.js
+++ discord-bot/src/index.js
@@ -177,3 +177,4 @@ bot.login(config.DISCORD_BOT_TOKEN).catch((err) => {
 // 7. Graceful Shutdown
 for (const signal of ["SIGTERM", "SIGINT"]) {
   process.on(signal, () => {
+    console.log(`\n[Shutdown] Received ${signal}. Closing Discord bot connection...`);
