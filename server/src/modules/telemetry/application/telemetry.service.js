cbbde77e199ded5163a2307d4277a5181fbc2bc7
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:54 +0400

    refactor(telemetry): validate event timestamp boundaries

--- server/src/modules/telemetry/application/telemetry.service.js
+++ server/src/modules/telemetry/application/telemetry.service.js
@@ -3,4 +3,8 @@ const { toTelemetryEvent } = require('../domain/telemetry.factory');
 class TelemetryService {
   constructor({ telemetryRepository }) {
     this.telemetryRepository = telemetryRepository;
+  }
+
+  async ingest(input) {
+    return this.telemetryRepository.save(toTelemetryEvent(input));
   }
