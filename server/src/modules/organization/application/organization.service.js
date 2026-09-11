709b740f3740e47dc4858a0be1d2d01a2bdba7c1
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:57 +0400

    refactor(org): add validateMemberRole permission check

--- server/src/modules/organization/application/organization.service.js
+++ server/src/modules/organization/application/organization.service.js
@@ -1,4 +1,9 @@
 const { ConflictError } = require('../../../shared/errors/app-error');
 const { createOrganization, slugifyOrganizationName } = require('../domain/organization.factory');
 
-class OrganizationService {
+class OrganizationService {
+  constructor({ organizationRepository }) {
+    this.organizationRepository = organizationRepository;
+  }
+
+  async create({ name }, transaction) {
