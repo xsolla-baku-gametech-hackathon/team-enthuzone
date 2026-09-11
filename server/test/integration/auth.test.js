ad129747825dd47e6c4bdc60ae81e23f580479b9
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:36:09 +0400

    test(integration): test unauthorized access rejection on protected endpoints

--- server/test/integration/auth.test.js
+++ server/test/integration/auth.test.js
@@ -9,4 +9,16 @@ const { createApp } = require('../../src/app');
 const { MemoryOrganizationRepository } = require('../../src/modules/organization');
 const { MemoryUserRepository } = require('../../src/modules/user');
 const { MemoryTransactionManager } = require('../../src/modules/auth');
-
+
+const secret = process.env.JWT_SECRET;
+const registration = {
+  organizationName: 'DarkFront Studio',
+  name: 'John Doe',
+  email: 'John@DarkFront.com',
+  password: 'StrongPassword123!',
+};
+
+function testContext() {
+  const organizationRepository = new MemoryOrganizationRepository();
+  const userRepository = new MemoryUserRepository();
+  const app = createApp({
