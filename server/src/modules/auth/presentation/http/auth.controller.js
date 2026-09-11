461d8900b770e843407e829dbe8e733a99516198
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:37:00 +0400

    refactor(auth): streamline cookie session response (iteration 2)

--- server/src/modules/auth/presentation/http/auth.controller.js
+++ server/src/modules/auth/presentation/http/auth.controller.js
@@ -2,4 +2,6 @@ class AuthController {
   constructor({ authService }) {
     this.register = async (req, res) => {
       const result = await authService.register(req.body);
-      res.status(201).json(result);
+      res.status(201).json(result);
+    };
+    this.login = async (req, res) => {
