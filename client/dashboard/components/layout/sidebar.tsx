6c584bb4ba63e053fe7699731a6150839cfe8142
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:26:07 +0400

    feat(layout): create Sidebar container component

---
+++ client/dashboard/components/layout/sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Activity,
+  GitCompareArrows,
+  LayoutDashboard,
+  MessageSquareText,
+  RadioTower,
+  ShieldAlert,
+} from "lucide-react";
+
+const nav = [
