353953eb8ec5ed0a7433c52f5ca616e5e48c0386
kenanqafarov <qafarovkenan06@gmail.com>
2026-09-11 04:37:02 +0400

    refactor(user): add findByIdWithOrganization helper (iteration 2)

--- server/src/modules/user/application/user.service.js
+++ server/src/modules/user/application/user.service.js
@@ -3,4 +3,7 @@ const { createUser, normalizeEmail } = require('../domain/user.factory');
 class UserService {
   constructor({ userRepository }) {
     this.userRepository = userRepository;
-  }
+  }
+
+  findByEmail(email, transaction) {
+    return this.userRepository.findByEmail(normalizeEmail(email), transaction);
